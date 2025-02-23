7350a824e5b43ad720953121a16ced1e93d1581e
nrxo777 <noorxcalma@gmail.com>
2025-02-23 23:29:34 +0600

    jsx_using_class_component

--- 01_JSX_Syntax/src/App.jsx
+++ 01_JSX_Syntax/src/App.jsx
@@ -1,44 +1,59 @@
-import React from "React";
+import React, { Component } from "React";
+import AnotherPlanet from "./components/AnotherPlanet";
 
-function App() {
-  // return (
-  //   <>
-  //     <div className="App">
-  //       <h1>Hello Planet</h1>
-  //       <Person />
-  //     </div>
-  //   </>
-  // );
+// function App() {
+//   return (
+//     <>
+//       <div className="App">
+//         <h1>Hello Planet</h1>
+//         <Person />
+//         <AnotherPlanet />
+//       </div>
+//     </>
+//   );
+// }
+// *** Same shit using Class Component ***
 
-  // *** Same shit using javascript ***
-
-  return React.createElement(
-    "div",
-    { className: "App" },
-    React.createElement("h1", null, "Hello Planet"),
-    <Person />
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Class bro</h1>
+        <Person />
+        <AnotherPlanet />
+      </div>
+    );
+  }
 }
 
+// *** Same shit using javascript ***
+
+//   return React.createElement(
+//     "div",
+//     { className: "App" },
+//     React.createElement("h1", null, "Hello Planet"),
+//     <Person />
+//   );
+
 const Person = () => {
-  //   return (
-  //     <>
-  //       <h2>This is the planet where people eat.</h2>
-  //     </>
-  //   );
-  // };
-
-  // *** Same shit using javascript ***
-
-  return React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h2",
-      null,
-      "This is another planet where people shit too!"
-    )
+  return (
+    <>
+      <h2>This is the planet where people eat.</h2>
+    </>
   );
 };
+
+// *** Same shit using javascript ***
+
+//   return React.createElement(
+//     "div",
+//     null,
+//     React.createElement(
+//       "h2",
+//       null,
+//       "This is another planet where people shit too!"
+//     )
+//   );
+// };
 
 export default App;
---
+++ 01_JSX_Syntax/src/components/AnotherPlanet.jsx
@@ -0,0 +1,9 @@
+const AnotherPlanet = () => {
+  return (
+    <div>
+      <h3>This is far away from your planet...</h3>
+    </div>
+  );
+};
+
+export default AnotherPlanet;
