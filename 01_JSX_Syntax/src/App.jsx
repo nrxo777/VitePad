294bff91d06fb458f8ea1fc2f9715780f42c5537
nrxo777 <noorxcalma@gmail.com>
2025-02-23 22:49:33 +0600

    replacing_jsx_format_with_js

--- 01_JSX_Syntax/src/App.jsx
+++ 01_JSX_Syntax/src/App.jsx
@@ -1,19 +1,43 @@
+import React from "React";
+
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Hello Planet</h1>
-        <Person />
-      </div>
-    </>
+  // return (
+  //   <>
+  //     <div className="App">
+  //       <h1>Hello Planet</h1>
+  //       <Person />
+  //     </div>
+  //   </>
+  // );
+
+  // *** Same shit using javascript ***
+
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement("h1", null, "Hello Planet"),
+    <Person />
   );
 }
 
 const Person = () => {
-  return (
-    <>
-      <h2>This is the planet where people eat.</h2>
-    </>
+  //   return (
+  //     <>
+  //       <h2>This is the planet where people eat.</h2>
+  //     </>
+  //   );
+  // };
+
+  // *** Same shit using javascript ***
+
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h2",
+      null,
+      "This is another planet where people shit too!"
+    )
   );
 };
 
