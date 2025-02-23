4a5d7b275a4a50eb03ef0b651530e1328e3d1ee3
nrxo777 <noorxcalma@gmail.com>
2025-02-23 22:23:10 +0600

    cleaning_up_vite

---
+++ 01_JSX_Syntax/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello Planet</h1>
+        <Person />
+      </div>
+    </>
+  );
+}
+
+const Person = () => {
+  return (
+    <>
+      <h2>This is the planet where people eat.</h2>
+    </>
+  );
+};
+
+export default App;
