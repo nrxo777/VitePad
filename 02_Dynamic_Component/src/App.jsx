d7c35b06a60eed9d3981c89b20343d25d56a95da
nrxo777 <noorxcalma@gmail.com>
2025-02-25 21:20:26 +0600

    changing_state_using_input

--- 02_Dynamic_Component/src/App.jsx
+++ 02_Dynamic_Component/src/App.jsx
@@ -1,24 +1,57 @@
+import React, { useState } from "react";
 import Book from "./components/Book";
-import Person from "./components/Person";
+
+let bn = document.querySelector("#bn");
 
 function App() {
+  const [books, setBooks] = useState([
+    { book_name: "Make Out", writer: "Master Jiraya" },
+    { book_name: "Get a life", writer: "Chloe Brown" },
+    { book_name: "After", writer: "Anna Todd" },
+  ]);
+
+  const changeName = (e) => {
+    const updatedBooks = [...books];
+    updatedBooks[0].book_name = e.target.value;
+    setBooks(updatedBooks);
+  };
+
+  const changeWriter = (e) => {
+    const updatewriter = [...books];
+    updatewriter[0].writer = e.target.value;
+    setBooks(updatewriter);
+  };
+
+  const submit = (e) => {
+    console.log(bn.value);
+  };
+
   return (
     <>
-      {/* <h2>hey planet! {parseInt(Math.random() * 100)}</h2>
-      <h2>hey planet! {parseInt(Math.random() * 100)}</h2>
-      <h2>hey planet! {parseInt(Math.random() * 100)}</h2> */}
-      {/* <Person name="Naruto" age="22">
-        I'm a goody goody boy
+      <div>
+        {/* <h2>hey planet! {parseInt(Math.random() * 100)}</h2>
+        <h2>hey planet! {parseInt(Math.random() * 100)}</h2>
+        <h2>hey planet! {parseInt(Math.random() * 100)}</h2> */}
+        {/* <Person name="Naruto" age="22">
+          I'm a goody goody boy
         </Person>
         <Person name="Luffy" age="19" />
         <Person name="Ichigo" age="17" />
         <Person name="Goku" age="27" /> */}
+      </div>
+
       <h1>Book List</h1>
       <hr />
+      <form action="">
+        <input type="text" placeholder="Book Name" onChange={changeName} />
+        <input type="text" placeholder="Writer Name" onChange={changeWriter} />
+        <input type="submit" name="Submit" onChange={submit} />
+      </form>
+
       <hr />
-      <Book book_name="Make Out" writer="Master Jiraya" />
-      <Book book_name="Get a life" writer="Chloe Brown" />
-      <Book book_name="After" writer="Anna Todd" />
+      {books.map((book, index) => (
+        <Book key={index} book_name={book.book_name} writer={book.writer} />
+      ))}
     </>
   );
 }
--- 02_Dynamic_Component/src/components/Book.jsx
+++ 02_Dynamic_Component/src/components/Book.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Book = (e) => {
-  console.log(e);
+  // console.log(e);
   return (
     <>
       <h3>
         Book Name: <span>{e.book_name}</span>
       </h3>
       <h4>
-        Write: <span>{e.writer}</span>
+        Writer: <span>{e.writer}</span>
       </h4>
       <hr />
     </>
